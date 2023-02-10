1b64c509459d00f3c51682d40ea38f67bfa99916
Nayara <nayaralima@poli.ufrj.br>
2023-02-10 13:27:38 -0300

    fix: basURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL, 
+  baseURL: 'http://localhost:5000/', 
 });
 
 export function createHeaders() {
